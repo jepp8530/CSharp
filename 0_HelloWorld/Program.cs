a083001d8ac1da4082288b06ae48425e5a4fb2e5
jepp8530 <jepp8530@mail.hansenberg.dk>
2022-08-25 16:01:40 +0100

    Gamer

--- 0_HelloWorld/Program.cs
+++ 0_HelloWorld/Program.cs
@@ -30,48 +30,68 @@ comment
 //string ExampleString = "Example 123";
 #endregion
 #region 
+Console. ForegroundColor = ConsoleColor. Blue;
 Console.Title = "TES Hansenberg";
 
+ Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("1. New Game\n" +
+                          "2. Load Game\n" +
+                          "3. Exit Game\n");
+        var choice = Console.ReadLine();
+        var correctChoice = false;
+        while (!correctChoice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    correctChoice = true;
+                    break;
+                case "2":
+                    Console.WriteLine("This feature is not yet implemented.");
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Please enter a correct value.");
+                    break;
+            }
 
-Console.WriteLine("Are you ready to start your adventure?");
-
-string answer = Console.ReadLine();
-
-bool AdventureStart = false;
-if (answer == "Yes")
-{
-    AdventureStart = true;
-}
-else if (answer == "No")
-{
-
-}
-
-
-
-
-if (AdventureStart == true)
-{
-    Console.WriteLine("Create your character.");
+            if (!correctChoice)
+            {
+                choice = Console.ReadLine();
+            }
+        }
+    
+    Console.WriteLine("Create your character:");
+    Thread.Sleep(200);
     Console.WriteLine("What is your name?");
-
+    Thread.Sleep(200);
     string name = "This is how to define a string variable!";
 
     name = Console.ReadLine();
 
     Console.WriteLine("Welcome " + name + ", you sound strong!");
-
-    Console.WriteLine("Choose your race...\n-Human\n-Elf\n-Orc\n-Dwarf\n-Wolfman");
+    Thread.Sleep(500);
+    Console.WriteLine("Choose your race..." + 
+    "\n1. Human" +
+    "\n2. Elf" +
+    "\n3. Orc" +
+    "\n4. Dwarf" +
+    "\n5. Wolfman");
 
     string race = "This is how to define a string variable!";
 
     race = Console.ReadLine();
     Console.WriteLine("Where are you from " + race + "?");
+    Thread.Sleep(500);
 
-
-    answer = Console.ReadLine();
+    Thread.Sleep(500);
+    Console.WriteLine("Choose your place of birth...\nThe \nThe warm Deserts of Voalen\nThe swampy Wetlands of Saerthem\nDwarf\nWolfman");
 
     Console.WriteLine("...Your adventure begins now...");
+    Console.ReadLine();
+    Console.WriteLine("You awaken in a foreign place with no recolection of arriving.");
+
     Console.ReadLine(); //Stops program from exiting until we press a key
-}
+
 #endregion
