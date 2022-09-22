880bcaa947b03454bff36510f21e15f1652ca0d6
jepp8530 <jepp8530@mail.hansenberg.dk>
2022-09-22 10:03:53 +0100

    Fibonacci

--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,21 +1,29 @@
 ﻿//Un-comment below code and fix the issues
 //Push to github
-/*
+
+Console.WriteLine("How many fibonacci bumbers do you want?");
+
+
+int loops = int.Parse(Console.ReadLine());
+
 int RecursiveFibonacci(int first, int second, int count)
 {
     count--;
     
+    int next = first + second;
+    first = second;
+    second = next;
     
     if(count == 0) {
+        return next;
     
     }else{
-        return RecursiveFibonacci(?, ?, ?);
+        return RecursiveFibonacci(first, second, count);
     }
 }
 
-int lastFibo = RecursiveFibonacci(1,1,5);
-Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
-*/
+int lastFibo = RecursiveFibonacci(1,1,loops);
+Console.WriteLine("Last fibonacci number was " + lastFibo);
+Console.ReadLine();
 
 
